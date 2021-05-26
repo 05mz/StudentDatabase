c46b9e8fe86dca734b370d97e5cab93fe026d004
Zaid Mohammad <74952827+05mz@users.noreply.github.com>
2021-05-26 09:52:13 +0530

    Delete student_data_base/student_data_base directory

--- student_data_base/student_data_base/text.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost', 'nafey', '1234', 'student_data_base');
-if(!$conn){
-	echo 'Connection error: '. mysqli_connect_error();
-}
-
-?>
--- student_data_base/student_data_base/x.php
+++
@@ -1,9 +0,0 @@
-<?php
-$x = 10;
-$y = 11;
-if ($x < $y) {
-?>
-      <h1>hello world</h1>
-<?php
-}
-?>
